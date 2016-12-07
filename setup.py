831afd9468413cfe0e443eca14fe4c2b990aabb0
Rok Roskar <roskarr@ethz.ch>
2016-12-07 17:22:57 +0100

    changes to setup

--- setup.py
+++ setup.py
@@ -9,11 +9,15 @@ currdir = os.getcwd()
 
 setup(name="abnet",
       author="Rok Roskar",
+      version='0.1',
       author_email="roskar@ethz.ch",
+      url="http://github.com/rokroskar/abnet",
       package_dir={'abnet/': ''},
       packages=['abnet'],
       ext_modules = cythonize([Extension("abnet.process_strings_cy", 
                                sources=["abnet/process_strings_cy.pyx"], 
                                include_dirs=[numpy.get_include()])]),
-      scripts=['scripts/abnet-analyze']
+      scripts=['scripts/abnet-analyze'],
+      install_requires=['click', 'findspark'],
+      keywords=['pyspark','genomics', 'hpc','bioinformatics']
 )
