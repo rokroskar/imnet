5870e323afbffd939815df3d38bae8c53f2a7d93
Uwe <uwe.schmitt@id.ethz.ch>
2018-02-06 10:16:05 +0100

    imnet now also works with python 3

--- imnet/__init__.py
+++ imnet/__init__.py
@@ -1,7 +1,7 @@
+from __future__ import absolute_import
+
 from . import process_strings
 from . import process_strings_cy
 from . import random_strings
 
-from process_strings import process_file, \
-                            write_output
-                            
+from .process_strings import process_file, write_output
